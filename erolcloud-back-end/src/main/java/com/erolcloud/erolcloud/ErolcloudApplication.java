6c1c4ac09286635976a1b619dd07d98030c11caa
Doruk Kantarcıoğlu <dorukkantarcioglu@gmail.com>
2023-05-21 13:04:31 +0300

    Initial back-end code push

---
+++ erolcloud-back-end/src/main/java/com/erolcloud/erolcloud/ErolcloudApplication.java
@@ -0,0 +1,13 @@
+package com.erolcloud.erolcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErolcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErolcloudApplication.class, args);
+	}
+
+}
---
+++ erolcloud-back-end/src/test/java/com/erolcloud/erolcloud/ErolcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erolcloud.erolcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErolcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
