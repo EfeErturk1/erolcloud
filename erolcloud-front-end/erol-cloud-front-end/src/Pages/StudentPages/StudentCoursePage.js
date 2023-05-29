aa7863e268389fae336614c560e0f4d856af477f
Çağla Ataoğlu <63456419+cagla-ataoglu@users.noreply.github.com>
2023-05-29 22:14:46 +0300

    Add unenroll from course

--- erolcloud-front-end/erol-cloud-front-end/src/Pages/StudentPages/StudentCoursePage.js
+++ erolcloud-front-end/erol-cloud-front-end/src/Pages/StudentPages/StudentCoursePage.js
@@ -53,16 +53,17 @@ const StudentCoursePage = () => {
         setSearchInput(event.target.value)
     }
 
-    const isEnrolled = (course) => {
+    const isEnrolled = (courseId) => {
         for (let i = 0; i < enrolledCourses.length; i++) {
-            if (enrolledCourses[i].id === course.id)
+            if (enrolledCourses[i].id === courseId)
                 return true
         }
         return false
     }
 
-    const handleEnroll = async (courseId, isEnrolled) => {
-        if (!isEnrolled) {
+    const handleEnroll = async (courseId) => {
+        const enrolled = isEnrolled(courseId)
+        if (!enrolled) {
             try {
                 const response = await fetch('http://localhost:8080/api/v1/students/enrollments', {
                     method: 'PUT',
@@ -75,16 +76,36 @@ const StudentCoursePage = () => {
                         'courseId': courseId
                     })
                 })
-                const course = await response.json()
-                if (response.ok)
+                if (response.ok) {
+                    const course = await response.json()
                     window.alert(`Enrolled in course ${course.code}-${course.section}`)
+                }
                 window.location.reload()
             } catch (e) {
                 console.log('Error enrolling in course:', e)
             }
         } else {
-            console.log(`Unenrolled from course ${courseId}`)
-            // TODO unenrolling
+            try {
+                const response = await fetch('http://localhost:8080/api/v1/students/enrollments', {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-type': 'application/json',
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({
+                        'studentId': studentId,
+                        'courseId': courseId
+                    })
+                })
+                if (response.ok) {
+                    const course = await response.json()
+                    window.alert(`Unenrolled from course ${course.code}-${course.section}`)
+                }
+                window.location.reload()
+            } catch (e) {
+                console.log('Error unenrolling from course:', e)
+            }
+
         }
     }
 
@@ -121,10 +142,10 @@ const StudentCoursePage = () => {
                             </div>
                             <div className='enroll-button-div'>
                                 <button
-                                    onClick={() => handleEnroll(course.id, isEnrolled(course))}
-                                    className={`btn btn-primary ${isEnrolled(course) ? 'enroll-button-gray' : 'enroll-button'}`}
+                                    onClick={() => handleEnroll(course.id, isEnrolled(course.id))}
+                                    className={`btn btn-primary ${isEnrolled(course.id) ? 'enroll-button-gray' : 'enroll-button'}`}
                                 >
-                                    {isEnrolled(course) ? 'Unenroll from course' : 'Enroll in course'}
+                                    {isEnrolled(course.id) ? 'Unenroll from course' : 'Enroll in course'}
                                 </button>
                             </div>
                         </li>
